3b45120e8f82528d49123c254f0f545a3bca98a1
Tommi Lepola <lepola.tommi@gmail.com>
2019-02-03 12:53:41 +0200

    Add comments and way to use local settings in postgres

--- server.js
+++ server.js
@@ -9,15 +9,29 @@ const io = socketIO(server);
 
 const { Pool } = require('pg');
 
+const localSettings = require('./localSettings.json');
+console.log(localSettings);
+
+
+// Postgres local settings
+// const pool = new Pool(localSettings);
+
+// Postgres heroku settings
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
 });  
 
+/**
+ * Socket listener
+ */
 io.on('connection', socket => {
 
     console.log('Connection made, id: ', socket.id);
 
+    // When a client connects, if the client is the only client in the server,
+    // fetch the amount of clicks from the database and emit the initclicks
+    // event to the client.
     if (io.engine.clientsCount === 1) {
         console.log('Getting clicks from database');
         getClicksFromDatabase()
@@ -25,36 +39,54 @@ io.on('connection', socket => {
             socket.emit('initclicks', res.rows[0].clicks);
         })
     } else {
-        socket.broadcast.emit('requestClicks', socket.id);
+        // If the new client is not the only client, get the last client before the new client
+        // and send a request to it for the amount of clicks.
+        const targetSocket = Object.keys(io.sockets.sockets)[io.engine.clientsCount - 2];
+        io.to(`${targetSocket}`).emit('requestClicks', socket.id); // socket.id is the ID of the new client
     }
 
+    // When a new client connects, it sends a request for the clicks amount to the last client
+    // before the new client. After that the client which responds to the request emits a 
+    // 'newConnection' event with the amount of clicks and the id of the new client.
     socket.on('newConnection', (data) => {
         console.log(data);
-        io.to(`${data.socket}`).emit('initclicks', data.clicks);
+        io.to(`${data.socket}`).emit('initclicks', data.clicks); // Send the amount of clicks to the new client
     })
 
+    // When the socket disconnects, just log that the client has disconnected
     socket.on('disconnect', () => {
         console.log('User disconnected, id: ', socket.id);
     });
 
+    // When the client clicks, emit a broadcast with the amount of 
+    // clicks to the other clients.
     socket.on('clicked', (data) => {
         socket.broadcast.emit('clicked', data);
-        let win = countClicks(data);
+        let win = countClicks(data); // Check if the client has won something after the click event
         if (win !== null) {
-            let winAmount = countClicks(data);
-            sendWinToDatabase(winAmount, data);
-            socket.emit('win', 'You win ' + winAmount + '!');
+            sendWinToDatabase(win, data); // Send the win to the Postgres database
+            socket.emit('win', 'You win ' + win + '!'); // Let the client know that he/she has won something
         }
     });
 
-    socket.on('unload', (data) => {
-        if (io.engine.clientsCount === 1) {
+    // Called when a client e.g. closes the app.
+    socket.on('unload', (data) => { // Data has the amount of clicks currently
+        if (io.engine.clientsCount === 1) { // If the client was the last client connected...
             console.log(io.engine.clientsCount);
-            sendClicksToDatabase(data);
+            sendClicksToDatabase(data); // ... send the amount of clicks to database.
         }
     })
 });
 
+/**
+ * Checks if the client has won something.
+ * 
+ * Every 50th click wins big, every 20th click wins medium
+ * and every 10th click wins small. If there's overlapping wins,
+ * the client wins the biggest amount.
+ * 
+ * @param {Object} data 
+ */
 function countClicks(data) {
     if (data.clicks % 50 === 0) {
         return 'big';
@@ -67,6 +99,9 @@ function countClicks(data) {
     }
 }
 
+/**
+ * Get the amount of clicks from database.
+ */
 async function getClicksFromDatabase() {
     try {
         return await pool.query('SELECT clicks FROM clicks;');
@@ -75,6 +110,11 @@ async function getClicksFromDatabase() {
     };
 }
 
+/**
+ * Send the amount of clicks to database table 'clicks'.
+ * 
+ * @param {Object} data holds the amount of clicks in an object
+ */
 async function sendClicksToDatabase(data) {
     console.log('Sending clicks: '+data.clicks);
     try {
@@ -84,6 +124,12 @@ async function sendClicksToDatabase(data) {
     }
 }
 
+/**
+ * Sends a win to a database table 'win'.
+ * 
+ * @param {string} winAmount either "big", "medium" or "small"
+ * @param {Object} data holds the user's name and the amount of clicks he's done
+ */
 async function sendWinToDatabase(winAmount, data) {
     console.log(data.user)
     console.log(data.clicks);
